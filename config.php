61c909c3d4522b80d6002c700ac39126f98be9c4
mosiah-andrade <mosiahandrade@gmail.com>
2025-10-06 12:10:44 -0300

    fix local bug

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+// Arquivo: /config.php (fora da public_html)
+
+return [
+    'db_host' => '193.203.175.197',
+    'db_name' => 'u103839941_HomologSolar',
+    'db_user' => 'u103839941_admin',
+    'db_pass' => 'Homo1007@',
+];
