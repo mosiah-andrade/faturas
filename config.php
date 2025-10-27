1924786d19083f8be2e2ae2bd0023ca800ca1617
mosiah-andrade <mosiahandrade@gmail.com>
2025-10-27 16:00:06 -0300

    deploy

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 
 // Defina o ambiente atual aqui: 'development' ou 'production'
 // Mude para 'production' quando for fazer o deploy do site.
-define('APP_ENV', 'development'); 
+define('APP_ENV', 'production'); 
 
 if (APP_ENV === 'production') {
     // Carrega as configurações de produção
